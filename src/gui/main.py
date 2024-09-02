7222b27cdc125c0f5075e2262abba4b260346748
tcstrength <thai.cuong1404@gmail.com>
2024-09-02 20:42:39 +0700

    update gui

--- src/gui/main.py
+++ src/gui/main.py
@@ -9,24 +9,13 @@ import streamlit as st
 from gui import api
 from gui.api import ModelArch
 
-st.title("23C01024 - 23C01025 - 23C01037")
+st.title("Hỗ trợ pháp luật lĩnh vực Tiền tệ ngân hàng".upper())
+st.subheader("MSSV: 23C01024 - 23C01025 - 23C01037")
 
-# Function to handle button click
-def set_active_button(model):
-    st.session_state.selected_button = model.value
-    
-if 'selected_button' not in st.session_state:
-    set_active_button(ModelArch.FINETUNE)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    if st.button(ModelArch.FINETUNE.value, key="btn1"):
-        set_active_button(ModelArch.FINETUNE)
-
-with col2:
-    if st.button(ModelArch.RAG.value, key="btn2"):
-        set_active_button(ModelArch.RAG)
+model_option = st.selectbox(
+    label="__Chọn mô hình:__",
+    options=(ModelArch.FINETUNE.value, ModelArch.RAG.value)
+)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -50,12 +39,11 @@ if prompt := st.chat_input("Nhập câu hỏi hoặc tình huống bạn đang g
         stream_response = api.post_chat_stream(
             user_input=prompt,
             history=st.session_state.messages,
-            model=st.session_state.selected_button)
+            model=model_option)
         for response in stream_response:
             full_text += response
             stream_placeholder.markdown(full_text)
-
-        st.markdown("Bạn có câu hỏi nào khác?")
+        stream_placeholder.markdown(full_text + f"(`Trả lời bằng {model_option}`)")
     # Add assistant response to chat history
     st.session_state.messages += [
         {"role": "user", "content": prompt},
